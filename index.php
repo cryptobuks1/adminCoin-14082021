16d3ce0a3239857a908c4541f394f58dda1b0a50
Hai LE Manh <hai.lemanh.1310@gmail.com>
2021-08-15 15:05:13 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,81 @@
 <?php $data["title"] = "Trang chủ"; ?>
 <?php $is_home = true; ?>
 <?php require "template-parts/layouts/header.php"; ?>
+<div class="uk-container uk-container-expand">
+    <div class="uk-section-small">
 
+        <div class="uk-margin myacc__started">
+            <h1 class="uk-h1 myacc__started__title">Get started, <span>Alex</span></h1>
+            <div class="myacc__started__desc">Here's your report overview by today.</div>
+        </div>
+
+        <!--Market overview-->
+        <div class="uk-margin myacc__market">
+            <h2 class="uk-h2 myacc__common__title">Market overview</h2>
+            <div class="uk-child-width-1-4@m uk-grid-small uk-grid-19-m" uk-grid>
+                <?php for ($i=1;$i<=4;$i++): ?>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body myacc__market__card">
+                        <div class="uk-position-top-right myacc__market__card__percent <?= ($i%2==0)? 'raise':'reduce' ?>">
+                            <span class="">2.06%</span>
+                        </div>
+                        <div class="uk-child-width-auto uk-grid-8 uk-flex-middle" uk-grid>
+                            <div><img class="myacc__market__card__logo" src="images/Coins/bitcoin@2x.png" alt=""></div>
+                            <div><span class="myacc__market__card__brandName">XLM/USDT</span></div>
+                            <div><i class="fa fa-info-circle myacc__market__card__info" aria-hidden="true"></i></div>
+                        </div>
+                        <div class="myacc__market__card__number" uk-grid>
+                            <div class="uk-width-1-3">
+                                <div class="myacc__market__card__number__txt1">0.33221</div>
+                                <div class="myacc__market__card__number__txt2">$0.332590</div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <canvas class="uk-visible@l" id="myChart<?= $i ?>" width="807" height="217"></canvas>
+                                <script>
+                                    var ctx = document.getElementById('myChart<?= $i ?>').getContext('2d');
+                                    var myChart = new Chart(ctx, {
+                                        type: 'line',
+                                        data: {
+                                            labels: ['', '', '', '', '', ''],
+                                            datasets: [{
+                                                label: '',
+                                                data: [<?= rand(10,1000) ?>, <?= rand(10,1000) ?>, <?= rand(10,1000) ?>, <?= rand(10,1000) ?>, <?= rand(10,1000) ?>, <?= rand(10,1000) ?>],
+                                                backgroundColor: [
+                                                    'rgba(255, 99, 132, 0.2)',
+                                                    'rgba(54, 162, 235, 0.2)',
+                                                    'rgba(255, 206, 86, 0.2)',
+                                                    'rgba(75, 192, 192, 0.2)',
+                                                    'rgba(153, 102, 255, 0.2)',
+                                                    'rgba(255, 159, 64, 0.2)'
+                                                ],
+                                                borderColor: [
+                                                    'rgba(255, 99, 132, 1)',
+                                                    'rgba(54, 162, 235, 1)',
+                                                    'rgba(255, 206, 86, 1)',
+                                                    'rgba(75, 192, 192, 1)',
+                                                    'rgba(153, 102, 255, 1)',
+                                                    'rgba(255, 159, 64, 1)'
+                                                ],
+                                                borderWidth: 1
+                                            }]
+                                        },
+                                        options: {
+                                            scales: {
+                                                y: {
+                                                    beginAtZero: true
+                                                }
+                                            }
+                                        }
+                                    });
+                                </script>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php endfor; ?>
+            </div>
+        </div>
+        <!--/Market overview-->
+    </div>
+</div>
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/header.php
+++ template-parts/layouts/header.php
@@ -13,6 +13,7 @@
     <!--JS-->
     <script src="https://cdn.jsdelivr.net/npm/@shinsenter/defer.js/dist/defer_plus.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.5.0/chart.min.js" integrity="sha512-asxKqQghC1oBShyhiBwA+YgotaSYKxGP1rcSYTDrB0U6DxwlJjU59B67U8+5/++uFjcuVM8Hh5cokLjZlhm3Vg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- UIkit JS -->
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
@@ -20,3 +21,86 @@
 </head>
 <body class="">
 <section id="app" class="uk-height-viewport uk-offcanvas-content uk-overflow-hidden uk-position-relative">
+<nav id="header" class="uk-navbar-container header" uk-navbar>
+
+    <div class="uk-navbar-left">
+        <a class="uk-navbar-toggle header__menu" uk-navbar-toggle-icon href="#"></a>
+        <a href="" class="uk-navbar-item uk-logo uk-padding-remove-left"><img class="header__logo" src="images/SIB_Logo-16.png" alt=""></a>
+    </div>
+
+    <div class="uk-navbar-right">
+
+        <ul class="uk-navbar-nav">
+            <li>
+                <a href="#">
+                    <div class="uk-cover-container uk-border-circle header__navbarRight__flag">
+                        <img src="https://cdn.britannica.com/33/4833-004-828A9A84/Flag-United-States-of-America.jpg" alt="" uk-cover>
+                        <canvas width="23" height="23"></canvas>
+                    </div>
+                </a>
+                <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+</nav>
+<div uk-height-viewport="offset-top: true" class="uk-grid-collapse uk-grid-match" uk-grid>
+    <aside class="sidebar uk-width-auto">
+        <div style="width: 190px" class="uk-card uk-card-default">
+            <ul class="uk-nav uk-nav-default">
+                <?php
+                $data = array(
+                    array(
+                        'name' => 'Home',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'My account',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'Wallet',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'Investment',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'Commission',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'Security',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'Parnership',
+                        'icon' => '',
+                    ),
+                    array(
+                        'name' => 'Setting',
+                        'icon' => '',
+                    ),
+                );
+                foreach ($data as $k => $v): ?>
+                <li>
+                    <a href="#">
+                        <img src="" alt="">
+                        <span><?= $v['name'] ?></span>
+                    </a>
+                    <span class="uk-tooltip"><?= $v['name'] ?></span>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </aside>
+    <div class="uk-width-expand">
+        <div class="uk-overflow-auto">
